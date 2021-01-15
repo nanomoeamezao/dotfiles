a3bb233d75aed0b04f5c2fa24cb96c29431b3cdd
neozumm <neozumm@rambler.ru>
2021-01-15 17:13:53 +0300

    autorun update

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -14,12 +14,7 @@ function run {
 if (command -v start-pulseaudio-x11 && ! pgrep pulseaudio); then
     start-pulseaudio-x11 &
 fi
-if (command -v /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 && ! pgrep polkit-mate-aut) ; then
-    /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
-fi
-if (command -v  xfce4-power-manager && ! pgrep xfce4-power-man) ; then
-    xfce4-power-manager &
-fi
+
 # System-config-printer-applet is not installed in minimal edition
 if (command -v system-config-printer-applet && ! pgrep applet.py ); then
   system-config-printer-applet &
@@ -29,13 +24,13 @@ fi
 run nm-applet
 run light-locker
 run picom -b
-run lxpolkit
+#run lxpolkit
 run batterymon -t numix 
-run batsignal -f 95 -c "CHARGE ME FAGGOT"
+run batsignal -f 95 -C "CHARGE ME FAGGOT" -m 320 -e -b
 # run xcape -e 'Caps_Lock=Escape'
 run thunar --daemon
 # run pa-applet
 # run pamac-tray
 # blueman-applet and msm_notifier are not installed in minimal edition
 run blueman-applet
-run msm_notifier
+# run msm_notifier
