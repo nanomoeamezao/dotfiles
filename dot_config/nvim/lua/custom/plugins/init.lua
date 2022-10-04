34197b52f0d22cb40a6114119929075a8fbaab77
neozumm <neozumm@rambler.ru>
2022-10-04 23:00:41 +0600

    nvim list session lens

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -383,4 +383,12 @@ return {
       }
     end,
   },
+  ["rmagatti/session-lens"] = {
+    requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+    after = "telescope.nvim",
+    config = function()
+      require("session-lens").setup {}
+      require("telescope").load_extension "session-lens"
+    end,
+  },
 }
